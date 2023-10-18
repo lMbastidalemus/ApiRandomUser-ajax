ece1719564bd7dd9d066ce78d632c116477b6124
lMbastidalemus <lbastidalemus@gmail.com>
2023-10-18 11:01:08 -0600

    Add project files.

---
+++ PL/Scripts/User/RandomUser.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    GetAll();
+});
+
+
+function GetAll() {
+        $.ajax({
+            type: "GET",
+            url: "https://randomuser.me/api/",
+          
+            success: function (result) {
+                var filas =
+                    "<tr>"
+                    + "<td class='text-center'>" + result.results[0].name.title + " " + result.results[0].name.first + " " + result.results[0].name.last + "</td>"
+                    + "<td class='text-center'>" + result.results[0].email + "</td>"
+                    + "<td class='text-center'>" + result.results[0].location.street.number + "" + result.results[0].location.street.name + "</td>"
+                    + "<td class='text-center'>" + result.results[0].gender + "</td>"
+                    + "<td class='text-center'>" + "<img src=" + result.results[0].picture.large + ">" + "</td>"
+                    + "</tr>";
+                $("#table tbody").append(filas);
+              
+            },
+            error: function (result) {
+                alert('Error al consultar' + result.responseJSON.ErrorMessage);
+            }
+        });
+};
+
