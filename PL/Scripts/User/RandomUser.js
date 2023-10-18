0b626e2efa76ffa23cf8189afc804e8832ca508d
lMbastidalemus <lbastidalemus@gmail.com>
2023-10-18 11:18:11 -0600

    Funcion limpiar

--- PL/Scripts/User/RandomUser.js
+++ PL/Scripts/User/RandomUser.js
@@ -1,5 +1,8 @@
-﻿$(document).ready(function () {
+﻿
+
+$(document).ready(function () {
     GetAll();
+  
 });
 
 
@@ -25,4 +28,9 @@ function GetAll() {
             }
         });
 };
+
+function Limpiar() {
+    $('#registros').empty();
+    
+}
 
